5bf3e3590ccdcaba44afdbc67eb1bd3fb321886a
Lina Vansssa S <blackcloverq999@gmail.com>
2025-05-20 12:32:44 -0500

    Final vistas casual y controller
    se terminan las vistas de casual y controlador

--- orderweb/resources/views/order/index.blade.php
+++ orderweb/resources/views/order/index.blade.php
@@ -1,11 +1,11 @@
 @extends('templates.base')
-@section('title', 'Causales')
-@section('header', 'Causales')
+@section('title', 'Órdenes')
+@section('header', 'Órdenes')
 @section('content')
 
 <div class="row">
     <div class="col-lg-12 mb-4 d-grid gap-2 d-md-block">
-        <a href="{{ route('causal.create') }}" class="btn btn-primary">Crear</a>
+        <a href="{{ route('order.create') }}" class="btn btn-primary">Crear</a>
     </div>
 </div>
 
@@ -17,25 +17,31 @@
             <thead>
                 <tr>
                 <th>Id</th>
-                <th>Descripción</th>
+                <th>Fecha legalización</th>
+                <th>Dirección</th>
+                <th>Ciudad</th>
+                <th>Causal</th>
+                <th>Observación</th>
                 <th>Acciones</th>
             </tr>
             </thead>
             <tbody>
-                @foreach ($causals as $causal)
                 <tr>
-                    <td>{{ $causal["id"] }}</td>
-                    <td>{{ $causal["description"] }}</td>
+                    <td>1</td>
+                    <td>20/05/2025</td>
+                    <td>Cra 28a #27-23</td>
+                    <td>TULUA</td>
+                    <td>Reparacion contador</td>
+                    <td>Perro bravo</td>
                     <td>
-                        <a href="{{ route('causal.edit', $causal["id"]) }}"  class="btn btn-primary btn-circle btn-sm" title="Editar">
+                        <a href="#"  class="btn btn-primary btn-circle btn-sm" title="Editar">
                             <i class="far fa-edit"></i>
                         </a>
-                        <a href="{{ route('causal.destroy', $causal["id"]) }}" class="btn btn-danger btn-circle btn-sm" title="Eliminar" onclick="return remove();">
+                        <a href="#" class="btn btn-danger btn-circle btn-sm" title="Eliminar" onclick="return remove();">
                             <i class="fas fa-trash"></i>
                         </a>
                     </td>
                 </tr>
-                @endforeach
             </tbody>
         </table>
     </div>
