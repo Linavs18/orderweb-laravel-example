c41d5d9367fbbfc74506d2e1ee235fbac3d48887
Lina Vansssa S <blackcloverq999@gmail.com>
2025-06-10 09:45:28 -0500

    reportes P2
    se termina reporte de tecnicos y se crea reporte de actividades por técnico

--- orderweb/app/Http/Controllers/ReportController.php
+++ orderweb/app/Http/Controllers/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
 use App\Models\Technician;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
@@ -10,7 +11,9 @@ class ReportController extends Controller
 {
     public function index()
     {
-        return view('reports.index');
+        $technicians = Technician::all();
+        return view('reports.index', compact('technicians'));
+
     }
 
     /**
@@ -23,8 +26,30 @@ class ReportController extends Controller
             'technicians' => $technicians
         );
 
-        $pdf = Pdf::loadView('reports.export_technicians', $data)->setPaper('letter', 'portrait'); //landscape: horizontal
+        /**
+         * dompdv version 3.x
+         * se debe agregar el setOptions
+         */
+        $pdf = Pdf::loadView('reports.export_technicians', $data)->setPaper('letter', 'portrait')
+        ->setOptions(['defaultFont' =>'sans-serif', 'isRemoteEnabled' => true]); //landscape: horizontal
 
         return $pdf->download('technicians.pdf');
+    }
+
+    /**
+     * Reporte que genera el listado de actividades de un técnico
+     */
+    public function export_activities_by_technician(Request $request)
+    {
+        $activities = Activity::where('technician_id', $request['technician_id'])->get();
+        
+        $data = array(
+            'activities' => $activities
+        );
+
+        $pdf = Pdf::loadView('reports.export_activities_by_technician', $data)->setPaper('letter', 'portrait')
+        ->setOptions(['defaultFont' =>'sans-serif', 'isRemoteEnabled' => true]); //landscape: horizontal
+
+        return $pdf->download('ActivitiesByTechnian-'. $request['technician_id']. '.pdf');
     }
 }
